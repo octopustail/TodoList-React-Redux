88f6557dfe057a8b0b634782e9f81a2b0e20b71f
octopustail <494479914@qq.com>
2018-06-21 22:06:04 +0800

    components 完成

---
+++ todo-list/src/components/Todo.react.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropType from 'prop-types'
+
+const Todo = ({text, onClick, isCompleted}) => (
+    <li onClick={onClick}
+        style={{textDecoration: isCompleted ? "line-though" : 'none'}}>
+        {text}
+    </li>
+);
+
+Todo.PropTypes = {
+    onClick: PropType.func.isRequired,
+    text: PropType.string.isRequired,
+    isCompleted: PropType.bool.isRequired
+};
+
+export default Todo
+
